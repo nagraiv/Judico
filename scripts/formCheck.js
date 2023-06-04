3357a357047784be56a7c2d49f7952f3d1c03f00
GraN <117268180+nagraiv@users.noreply.github.com>
2023-06-04 11:14:48 +0700

    btn disabled check

--- scripts/formCheck.js
+++ scripts/formCheck.js
@@ -2,28 +2,87 @@
     "use strict"
 
     const requestForm = document.querySelector('.request__form');
-    const submitBtn = requestForm.querySelector('.btn--form');
 
-    const collectData = function (formEl) {
-        const data = Array.from(formEl.elements)
-            .filter((item) => !!item.name)  // фильтруем эл-ты с пустым именем (кнопка)
-            .map((el) => { return {name: el.name, value: el.value } });
-        console.log(data);
+    const collectData = function(formEl) {
+        // const data = Array.from(formEl.elements)
+        //     .filter((item) => !!item.name)  // фильтруем эл-ты с пустым именем (кнопка)
+        //     .map((el) => {
+        //         const {name, value} = el;
+        //         return { name, value };
+        //     });
+        // console.log(data);
 
-        // newArray.forEach((item) => {
-        //     const { name, value } = item;
-        //     console.log({ name, value });
-        //
-        //     console.log(item.name, item.value);
-        // })
+        const data = new FormData();
+        Array.from(formEl.elements)
+            .filter((item) => !!item.name)
+            .forEach((el) => {
+                const {name, value} = el;
+                data.append(name, value);
+            });
+
+        return data;
     }
 
-    const handleSubmit = function (event) {
+    const sendData = async function(data) {
+        return await fetch('/api/apply/', {
+            method: 'POST',
+            headers: { 'Content-Type': 'multipart/form-data' },
+            body: data,
+        })
+    }
+
+    function toggleLoader() {
+        const loader = document.getElementById('loader');
+        loader.classList.toggle('none');
+    }
+
+    function onSuccess(formEl) {
+        alert('Thanks for your request!');
+        formEl.reset();
+    }
+
+    function onError(status, error) {
+        if (error && error.message) {
+            alert(error.message);
+        } else {
+            alert('The error occurred: ' + status);
+        }
+    }
+
+    const handleSubmit = async function(event) {
         event.preventDefault();
-        collectData(requestForm);
+        const data = collectData(event.target); // event.target = requestForm
+        // console.log(Array.from(data.entries()));
+        toggleLoader();
+        // const { response } = await sendData(data);
+        // console.log(response);
+        const { status, error } = await sendData(data);
+        // console.log(status, error);
+        toggleLoader();
+
+        if (status === 200) {
+            onSuccess(event.target);
+        } else {
+            onError(status, error);
+        }
     };
 
     // submitBtn.addEventListener('click', handleSubmit);
-    requestForm.addEventListener('submit', handleSubmit)
+    requestForm.addEventListener('submit', handleSubmit);
+
+    const disableBtn = function(event) {
+        const formEl = event.target.form;
+        const isValid = formEl.checkValidity();
+        formEl.querySelector('.btn--form').disabled = !isValid;
+
+        // const submitBtn = formEl.querySelector('.btn--form');
+        // if (!isValid){
+        //     submitBtn.setAttribute('disabled', true);
+        // } else {
+        //     submitBtn.removeAttribute('disabled');
+        // }
+    };
+
+    requestForm.addEventListener('input', disableBtn);
 
 })();
